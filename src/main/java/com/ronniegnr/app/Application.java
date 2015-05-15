2cb14c959c8e8a3204197ac175f349dfbba391fc
ronniegnr <gnr.ronnie@gmail.com>
2015-05-15 17:22:51 +0600

    jetty support

--- src/main/java/com/ronniegnr/app/Application.java
+++ src/main/java/com/ronniegnr/app/Application.java
@@ -2,16 +2,21 @@ package com.ronniegnr.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
-
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    private static Class<Application> applicationClass = Application.class;
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 }
